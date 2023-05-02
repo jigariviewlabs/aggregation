234b79df477f49ad613a29134616e782d4f78465
jigariviewlabs <jigar.prajapati@iviewlabs.net>
2023-05-02 15:54:27 +0530

    hello

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ItemController = require('../controller/aggregation');
 
+router.get('/', async (req,res) => {
+    res.send({"msg":"Hello World"})
+})
 router.post('/addItem',ItemController.itemdetails)
 router.post('/addUnivercity', ItemController.univercitydetails)
 router.post('/addCourse', ItemController.coursedetails)
